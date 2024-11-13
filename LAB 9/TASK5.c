eec9b47cda13f273bf66e18d353a62c030d7b0d4
ASHAR-alt <venomplays56@gmail.com>
2024-11-13 01:32:34 -0800

    Update TASK5.c

--- LAB 9/TASK5.c
+++ LAB 9/TASK5.c
@@ -1,19 +1,25 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<string.h>
 int main()
 {
-	int l,len_plaintext;
-	char plaintext[]="QUANVOLUTIONAL NEURAL NETWORK";
+	int l,i,len_plaintext;
+	char plaintext[50];
 	char key[30];
+	printf("Enter Text To Encrypt : \n");
+	fgets(plaintext,sizeof(plaintext),stdin);
+	plaintext[strcspn(plaintext,"\n")]='\0';
+	printf("Enter Key : \n");
+	scanf("%s",key);	
 	char final_cipher[30];
 	len_plaintext=strlen(plaintext);
 	int plaintext1[len_plaintext];
 	int key1[len_plaintext],cipher[len_plaintext];
-	l = strlen("QUBITS");	
-	for(int i=0 ; i<len_plaintext ; i++){
-		key[i]="QUBITS"[i%l];
+	l = strlen(key);	
+	printf("\nEncrypted Code : \n");
+	for( i=0 ; i<len_plaintext ; i++){
+		key[i]=key[i%l];
 	}
-	for(int i=0 ; i<len_plaintext ; i++){
+	for(i=0 ; i<len_plaintext ; i++){
 		if(plaintext[i]==' '){
 		plaintext1[i]=20;
 		key1[i]=key[i];	
@@ -21,7 +27,7 @@ int main()
 		plaintext1[i]=plaintext[i];
 		key1[i]=key[i];}
 			}
-	for(int i=0 ; i<len_plaintext ; i++){
+	for(i=0 ; i<len_plaintext ; i++){
 	 if(plaintext1[i]==20){
 	 	key1[i]-=64;
 	 }else{
@@ -30,22 +36,21 @@ int main()
 	 }
 
 	}
-	for(int i=0 ; i<len_plaintext ; i++){
+	for(i=0 ; i<len_plaintext ; i++){
 	cipher[i]=plaintext1[i]+key1[i];
 	}
 		
-	for(int i=0 ; i<len_plaintext ; i++){
+	for(i=0 ; i<len_plaintext ; i++){
 		cipher[i]=cipher[i]%26;
 		}
-		for(int i=0 ; i<len_plaintext ; i++){
+		for(i=0 ; i<len_plaintext ; i++){
 		cipher[i]=cipher[i]+64;
 		}
-		for(int i=0 ; i<len_plaintext ; i++){
+		for(i=0 ; i<len_plaintext ; i++){
 		   final_cipher[i]=cipher[i];
 		}
-			for(int i=0 ; i<len_plaintext ; i++){
+			for( i=0 ; i<len_plaintext ; i++){
 				printf("%c",final_cipher[i]);
 			}
 return 0;
 }
-
